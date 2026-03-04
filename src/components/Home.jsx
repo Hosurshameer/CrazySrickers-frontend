288d2db27619867d3d5a4e1853097074a246d699
Hosurshameer <shameerhosur29@gmail.com>
2026-03-04 11:58:47 +0530

    ... Added the lift state up for better searching

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,11 +9,16 @@ export default function Home() {
   const products = useLoaderData();
   const[fetchedProducts,setFetchedProducts]=useState(products);
   const[curPage,setCurPage]=useState(products.currentPage);
+  const[searchText,setSearchText]=useState("");
 
-  let flag=false;
-  const fetchPage=async(page)=>{
+ const handleSearch=((val)=>{
+  setSearchText(val);
+  fetchPage(0,val);
+ })
+
+  const fetchPage=async(page,keyword=searchText)=>{
   try{
-     const res=await apiClient.get(`/products?page=${page}&size=9`);
+     const res=await apiClient.get(`/products?keyword=${keyword}&page=${page}&size=9`);
      setFetchedProducts(res.data);
      setCurPage(res.data.currentPage);
   }catch(error){
@@ -32,19 +37,19 @@ export default function Home() {
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers.Perfect for any occasion!
       </PageHeading>
-      <ProductListings products={fetchedProducts.content} />
+      <ProductListings products={fetchedProducts.content} searchText={searchText} onSearch={handleSearch} />
     </div>
     <div className="flex justify-center gap-4 pb-12">
       <button 
       disabled={curPage===0}
-      onClick={()=>fetchPage(curPage-1)}
+      onClick={()=>fetchPage(curPage-1,searchText)}
       className="bg-primary dark:bg-light text-white dark:text-primary font-medium text-sm py-2 px-4 disabled:bg-gray-300 disabled:text-gray-600 rounded-md hover:cursor-pointer"
       >
         Prev</button>
            <span>Page {curPage + 1} of {fetchedProducts.totalPages}</span>
       <button
       disabled={curPage>=fetchedProducts.totalPages-1}
-      onClick={()=>fetchPage(curPage+1)}
+      onClick={()=>fetchPage(curPage+1,searchText)}
       className="bg-primary dark:bg-light text-white dark:text-primary font-medium text-sm py-2 px-4 disabled:bg-gray-300 disabled:text-gray-600 rounded-md hover:cursor-pointer"
       >
      Next
