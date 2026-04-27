21912ef9d032d143c91e5b83b903f807281dbe61
Hosurshameer <shameerhosur29@gmail.com>
2026-04-27 15:09:38 +0530

    modified and removed the content-type apllication/json from apiClient.js

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -6,7 +6,6 @@ import Cookies from "js-cookie";
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
-    "Content-Type":"application/json",
     Accept: "application/json",
   },
   timeout: 10000,
